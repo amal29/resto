1f769aa394e03bf334afd23a7bb758ef61dc8826
Amal sankar <amalsankar29@gmail.com>
2021-03-26 16:25:03 +0530

    mobile view

--- js/nav.js
+++ js/nav.js
@@ -1,13 +1,18 @@
 function openNav() {
   document.getElementById("myNav").style.width = "100%";
-//   document.getElementById("myNav").style.visibility = "visible";
+  //   document.getElementById("myNav").style.visibility = "visible";
+  console.log("hai opn");
+  document.getElementById("btnclose").style.display = "block";
 
   document.getElementById("btnid").style.display = "none";
 }
 
 function closeNav() {
-  document.getElementById("myNav").style.width = "0%";
+  console.log("hai close");
+  document.getElementById("myNav").style.display = "none";
   window.location.reload();
 }
+
+// document.getElementById("btnclose").style.display = "none";
 
 $(".js-counter").countTo();
